7abe35e6b04c691411897a0b2b50bac9959d9df4
Ariton Sefedini <aritonsefedini@icloud.com>
2021-01-21 00:03:23 -0500

    Wrote rspec

--- skeleton-2/spec/dessert_spec.rb
+++ skeleton-2/spec/dessert_spec.rb
@@ -6,37 +6,64 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 =end
 
 describe Dessert do
-  let(:chef) { double("chef") }
+  let(:chef) { double("chef"), name: "Pepper" }
+  let(:cookie) { Dessert.new("cookie", 15, chef) }
 
   describe "#initialize" do
-    it "sets a type"
+    it "sets a type" do
+      expect(cookie.type).to eq("cookie")
+    end
 
-    it "sets a quantity"
+    it "sets a quantity" do
+      expect(cookie.quantity).to eq(15)
+    end
 
-    it "starts ingredients as an empty array"
+    it "starts ingredients as an empty array" do
+      expect(cookie.ingredients).to be_empty
+    end
 
-    it "raises an argument error when given a non-integer quantity"
+    it "raises an argument error when given a non-integer quantity" do
+      expect { Dessert.new("reeses", "2", chef}.to raise_error(ArgumentError)
+    end
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    it "adds an ingredient to the ingredients array" do
+      cookie.add_ingredient("chocolate chips")
+      expect(cookie.ingredients).to include("chocolate chips")
+    end
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    it "shuffles the ingredient array" do
+      ingredients = "sugar", "brown sugar", "butter", "eggs", "chocolate chips"
+      ingredients.each do |ingredient|
+        cookie.add_ingredient(ingredient)
+    end
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
+    it "subtracts an amount from the quantity" do
+      cookie.eat(2)
+      expect(cookie.quantity).to eq(13)
+    end
 
-    it "raises an error if the amount is greater than the quantity"
+    it "raises an error if the amount is greater than the quantity" do
+      expect { cookie.eat(16)}.to raise_error("Not that many cookies left")
+    end
   end
 
   describe "#serve" do
-    it "contains the titleized version of the chef's name"
+    it "contains the titleized version of the chef's name" do
+      allow(chef).to receive(:titleize).and_return("Pepper")
+      expect(cookie.serve).to eq("Pepper baked 15 cookies!")
+    end
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      expect(chef).to receive(:bake).with(cookie)
+      cookie.make_more
+    end
   end
 end
